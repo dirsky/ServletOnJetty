d88a6aacc499d6223de450ac87bc9ddcfbf3e3fe
Frank <dirsky@126.com>
2019-09-19 11:24:50 +0800

    修改

--- src/main/java/com/frank/App.java
+++ src/main/java/com/frank/App.java
@@ -5,20 +5,20 @@ import org.eclipse.jetty.servlet.ServletHandler;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         Server server = new Server(8888);
+        //JVM退出时关闭Jetty
+        server.setStopAtShutdown(true);
 
         ServletHandler handler = new ServletHandler();
-        server.setHandler(handler);
-
         handler.addServletWithMapping(
                 HelloServlet.class, "/h");
+
+        server.setHandler(handler);
+
 
         try {
             server.start();
